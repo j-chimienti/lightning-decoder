76c97455a62f5d9255e9d0a52065b1f7b0c1da79
joe <joe.chimienti@eventdynamic.com>
2021-11-19 08:30:47 -0500

    test deploy

--- src/constants/app.js
+++ src/constants/app.js
@@ -3,4 +3,4 @@ export const APP_NAME = 'Lightning Decoder';
 export const APP_TAGLINE = 'Decode Lightning Network Requests';
 export const APP_SUBTAGLINE = 'BOLT11, LNURL, and Lightning Address'
 export const APP_INPUT_PLACEHOLDER = 'Enter Invoice';
-export const APP_GITHUB = 'https://github.com/andrerfneves/lightning-decoder';
+export const APP_GITHUB = 'https://github.com/j-chimienti/lightning-decoder';
